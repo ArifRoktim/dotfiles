bfbe99920bfee0c15e4aca266b33ab8bb11587da
Arif Roktim <aroktim@stuy.edu>
2017-11-03 22:26:20 -0400

    Add stuff to init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,8 +1,10 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Package stuff
-;; General setting
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  => Package stuff
+;;  => General setting
+;;  => Mappings
+;;  => Custom set variables
 
-;;;; Package stuff
+;; ======== Package stuff ========
 
 (package-initialize)
 (require 'package)
@@ -18,32 +20,83 @@
 (package-initialize) ;; You might already have this line
 
 (require 'evil)
+(require 'evil-leader)
 (require 'dracula-theme)
 (require 'key-chord)
 (require 'linum-relative)
 
-;;;; General setting
+;; ======== General setting ========
 
+;; Set modes
+(global-evil-leader-mode)
 (evil-mode 1)
 (key-chord-mode 1)
 
+;; Remove bloat
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
+(setq inhibit-startup-screen t)
+
+;; Highlight current line
+(global-hl-line-mode)
+
 ;; Relative line numbers
 (global-linum-mode)
 (linum-relative-global-mode)
 (setq linum-relative-current-symbol "")
 
+;; Use 4 spaces instead of tabs
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq tab-stop-list (number-sequence 4 120 4))
+
+;; if our source file uses tabs, we use tabs, if spaces spaces, and if        
+;; neither, we use the current indent-tabs-mode                               
+(defun infer-indentation-style ()
+  (let ((space-count (how-many "^  " (point-min) (point-max)))
+        (tab-count (how-many "^\t" (point-min) (point-max))))
+    (if (> space-count tab-count) (setq indent-tabs-mode nil))
+    (if (> tab-count space-count) (setq indent-tabs-mode t))))
+
+;; Keep 7 lines above and below cursor
+(setq scroll-margin 7)
+(setq scroll-step 1)
+(setq scroll-conservatively 200)
+
+;; Show matching parens
+(setq show-paren-delay 0)
+(show-paren-mode 1)
+
+;; No annoying bells
+(setq ring-bell-function 'ignore)
+
+;; Persistent undo
+(global-undo-tree-mode)
+(setq undo-tree-auto-save-history t)
+(setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
+
+;; Save cursor position
+(if (> emacs-major-version 24)
+    (save-place-mode 1)
+  ((require 'saveplace)
+   (setq-default save-place t)))
+
+;; ======== Mappings ========
+
 ;; Normal mode on jk
 (key-chord-define evil-insert-state-map  "jk" 'evil-normal-state)
+(define-key evil-insert-state-map (kbd "TAB") 'tab-to-tab-stop)
 
+;; ======== Custom set variables ========
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (linum-relative ## key-chord dracula-theme evil))))
+ '(package-selected-packages
+   (quote
+    (evil-leader linum-relative ## key-chord dracula-theme evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
